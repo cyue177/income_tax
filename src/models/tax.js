3b550a08eda6cb4533c02a94658a19aa81bcbeb2
Andy Qin <qinyang_1980@qq.com>
2018-10-26 16:40:41 +0800

    refine

--- src/models/tax.js
+++ src/models/tax.js
@@ -47,7 +47,7 @@ export default {
           old: oldTax,
           new: newTax,
           cut,
-          saving: API.toTwoFixed((oldTax - newTax))
+          saving: API.toTwoFixed(oldTax - newTax)
         }
       };
     },
